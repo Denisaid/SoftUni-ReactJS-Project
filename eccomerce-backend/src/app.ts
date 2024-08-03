62b32180ad4eb00c041f88f1a4f17c6b36c587d9
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2024-08-03 13:29:35 +0300

    Added_product_model,_create_product_controller_and_integrate

--- eccomerce-backend/src/app.ts
+++ eccomerce-backend/src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { connectDB } from './utils/features.js';
 import { config } from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
 
 import userRoute from './routes/user.js';
+import productRoute from './routes/products.js';
 
 config({
     path: './.env',
@@ -13,10 +15,17 @@ const mongoURI = process.env.MONGO_URI || '';
 
 connectDB(mongoURI);
 
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
+});
+
 const app = express();
 app.use(express.json());
 
 app.use('/api/v1/user', userRoute);
+app.use('/api/v1/product', productRoute);
 
 app.listen(port, () => {
     console.log(`Server is working on http://localhost:${port}`);
