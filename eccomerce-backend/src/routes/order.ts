578fc1089da8437eea6e3bbd32851226c7b4732d
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2024-08-04 06:32:57 +0300

    Added change order status and delete order controllers

--- eccomerce-backend/src/routes/order.ts
+++ eccomerce-backend/src/routes/order.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { allOrders, getSingleOrder, myOrders, newOrder } from '../controllers/order.js';
+import { allOrders, deleteOrder, getSingleOrder, myOrders, newOrder, processOrder } from '../controllers/order.js';
 import { adminOnly } from '../middlewares/auth.js';
 
 const app = express.Router();
@@ -7,6 +7,10 @@ const app = express.Router();
 app.post('/new', newOrder);
 app.get('/my', myOrders);
 app.get('/all', adminOnly, allOrders);
-app.route('/:id').get(getSingleOrder);
+app
+    .route('/:id')
+    .get(getSingleOrder)
+    .put(adminOnly, processOrder)
+    .delete(adminOnly, deleteOrder);
 
 export default app;
